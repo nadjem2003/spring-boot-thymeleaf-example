511beb7ab2c2ed44fe7213d0bfd481bd868c9955
nadjem217 <nadjem062@gmail.com>
2025-11-25 15:48:15 +0100

    ci testing

--- src/main/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplication.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootThymeleafExampleApplication {
 
+	// first comment test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafExampleApplication.class, args);
 	}
