87464d858a5e266d365646ccf266d0b6f4e32fcf
nadjem217 <nadjem062@gmail.com>
2025-11-26 11:15:11 +0100

    Ci pipeline configuration 1

--- src/main/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplication.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootThymeleafExampleApplication {
-
-	// first comment
-	// REAL first comment
-	// a Real comment because why not
-	// hope it works
+	// K
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafExampleApplication.class, args);
 	}
