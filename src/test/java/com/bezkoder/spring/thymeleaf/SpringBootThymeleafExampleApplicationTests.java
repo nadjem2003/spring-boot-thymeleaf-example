318fa07afc5c0814c5cdbf2696a86f44c5c150a1
nadjem217 <nadjem062@gmail.com>
2025-12-04 12:44:25 +0100

    Fix CI: use in-memory H2 database for tests

--- src/test/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplicationTests.java
+++ src/test/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplicationTests.java
@@ -2,8 +2,10 @@ package com.bezkoder.spring.thymeleaf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class SpringBootThymeleafExampleApplicationTests {
 
 	@Test
